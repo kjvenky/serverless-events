0019febe8d7c48e4eafcc3d50b9b6f9725b830ec
K J Venkatesh <kjvenky@gmail.com>
2017-12-30 18:24:48 +0530

    Update Projection

--- event-service/projections.js
+++ event-service/projections.js
@@ -7,18 +7,18 @@ var s3 = new AWS.S3();
 module.exports.manageEventData = (event, context, cb) => {
   // Is it possible for multiple SNS message delivered at once?
   let snsMessage = JSON.parse(event.Records[0].Sns.Message); 
+
+  // Handle Messages Based on the event
   let keyName = snsMessage.Records[0].s3.object.key;
   let bucketName = process.env.EVENT_STORE_BUCKET;
-
   let eventsDataBucket =  process.env.EVENTS_BUCKET;
+  let eventData;
 
   S3Handler.readFile(bucketName, keyName).then(function(eventData) {
-  	// Update the file based on the new event
-  	console.log(eventData)
-  	console.log(JSON.parse(eventData).eventId)
- 	return S3Handler.saveFile(eventsDataBucket, JSON.parse(eventData).eventId + '.json', eventData)
-  }).then(function(){
-  	cb(null, null);
+ 	return  S3Handler.readFile(eventsDataBucket, JSON.parse(eventData).eventId + '.json')
+  }).then(function(eventData){
+  	// Change only the fields updated
+  	return S3Handler.saveFile(eventsDataBucket, JSON.parse(currentPrjData).eventId + '.json', eventData)
   }).catch(function(err){
   	console.log(err)
   	cb(null, err);
